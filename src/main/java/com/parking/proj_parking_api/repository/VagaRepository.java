02d955c38de0c32f0a4d7923155e57c4fd11f153
Your Name <you@example.com>
2026-04-01 16:55:33 -0300

    Criando e mapeando a entidade vaga e o repositório

---
+++ src/main/java/com/parking/proj_parking_api/repository/VagaRepository.java
@@ -0,0 +1,9 @@
+package com.parking.proj_parking_api.repository;
+
+import com.parking.proj_parking_api.entity.Vaga;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VagaRepository extends JpaRepository <Vaga, Long>{
+
+    
+}
