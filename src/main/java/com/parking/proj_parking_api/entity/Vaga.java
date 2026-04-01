10fd219bbb8cc60dc633a41aabed88b028b6c203
Your Name <you@example.com>
2026-04-01 09:50:50 -0300

    Recursos de Vagas Reinicio

---
+++ src/main/java/com/parking/proj_parking_api/entity/Vaga.java
@@ -0,0 +1,20 @@
+package com.parking.proj_parking_api.entity;
+
+import jakarta.persistence.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import java.io.Serializable;
+
+@Entity
+@Table (name = "vagas")
+@EntityListeners (AuditingEntityListener.class)
+public class Vaga implements Serializable {
+
+@Id
+@GeneratedValue (strategy = GenerationType.IDENTITY)
+private Long Id;
+
+@Column (name = "codigo", nullable = false, unique = true, length = 4)
+private String codigo;
+
+
+}
