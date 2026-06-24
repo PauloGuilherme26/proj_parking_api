81855327ca12623e5269c08ac66462055378d1d9
Your Name <you@example.com>
2026-06-24 15:53:57 -0300

    [Seção_16-Recursos_de_Estacionamentos_Ok] Inserção do termo final nas variáveis de EstacionamentoService

--- src/main/java/com/parking/proj_parking_api/service/EstacionamentoService.java
+++ src/main/java/com/parking/proj_parking_api/service/EstacionamentoService.java
@@ -16,9 +16,9 @@ import lombok.*;
 @Service
 public class EstacionamentoService {
 
-    private ClienteVagaService clienteVagaService;
-    private ClienteService clienteService;
-    private VagaService vagaService;
+    private final ClienteVagaService clienteVagaService;
+    private final ClienteService clienteService;
+    private final VagaService vagaService;
 
     @Transactional
     public ClienteVaga checkIn (ClienteVaga clienteVaga) {
